1b85e8d76588eea6cf40cb5315f405495c998a27
Chinmay Deshpande <Chinmaydeshapande@gmail.com>
2026-08-13 22:24:16 +0530

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    
+    struct Compare {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val;
+        }
+    };
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+
+        priority_queue<ListNode*, vector<ListNode*>, Compare> pq;
+
+        // Put first node of every list into heap
+        for (ListNode* list : lists) {
+            if (list != nullptr) {
+                pq.push(list);
+            }
+        }
+
+        // Dummy node for easier merging
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+
+        while (!pq.empty()) {
+
+            // Get smallest node
+            ListNode* smallest = pq.top();
+            pq.pop();
+
+            // Add it to result
+            tail->next = smallest;
+            tail = tail->next;
+
+            // Add next node from the same list
+            if (smallest->next != nullptr) {
+                pq.push(smallest->next);
+            }
+        }
+
+        return dummy->next;
+    }
+};
